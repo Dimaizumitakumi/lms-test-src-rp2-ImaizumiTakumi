9bdd4caf9c6c0e09532fd7f217bb429373f8b2f3
t-imaizumi <timaizumi@ceate-inet.co.jp>
2026-09-18 16:13:14 +0900

    fix:Imaizumi Case01.javaテストコード修正
    utilが使えていなかったため、代替できる部分を入れ替え。エビデンス取得し直し。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -3,13 +3,6 @@ package jp.co.sss.lms.ct.f01_login1;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -17,8 +10,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト ログイン機能①
@@ -46,21 +37,12 @@ public class Case01 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		//アクセスおよびタイトル検証
-		webDriver.get("http://localhost:8080/lms");
+		goTo("http://localhost:8080/lms");
 		assertEquals("ログイン | LMS", webDriver.getTitle());
-		//スクリーンショット取得、保存処理
-		TakesScreenshot takesScreenshot = (TakesScreenshot) webDriver;
-		File file = takesScreenshot.getScreenshotAs(OutputType.FILE);
-		LocalDateTime localDateTime = LocalDateTime.now();
-		String localDateTimeStr = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-		String fileName = "Case01_" + localDateTimeStr + ".png";
 
-		try {
-			Files.copy(file.toPath(), Paths.get("./evidence/" + fileName));
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println(fileName + "は保存できませんでした。");
-		}
+		//スクリーンショット取得、保存処理
+		getEvidence(new Object() {
+		});
 	}
 
 }
