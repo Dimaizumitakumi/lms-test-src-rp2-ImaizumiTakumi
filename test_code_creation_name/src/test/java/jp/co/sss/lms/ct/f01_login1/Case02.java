b779d5748cba4156e41b9747c9a786d338bb1de5
t-imaizumi <timaizumi@ceate-inet.co.jp>
2026-09-18 13:30:45 +0900

    add:Imaizumi Case02.java ログイン画面への遷移テストコード追加
    WebElementを利用してログインIDとパスワードに異常値を入力し、ログイン処理を実行。実行後のエラーが期待値通りか確認するテストを作成した。
    入力値は以下の通り
    ログインID：Stranger001
    パスワード：Stranger001
    期待値は以下の通り
    エラーメッセージ：* ログインに失敗しました。
    タイトル：ログイン | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,26 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement idIdElement = webDriver.findElement(By.id("loginId"));
+		idIdElement.clear();
+		idIdElement.sendKeys("Stranger001");
+		WebElement idPwElement = webDriver.findElement(By.id("password"));
+		idPwElement.clear();
+		idPwElement.sendKeys("Stranger001");
+		WebElement cssBtnElement = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		cssBtnElement.click();
+		WebElement idErrorElement = webDriver.findElement(By.cssSelector(".help-inline.error"));
+		assertTrue(idErrorElement.isDisplayed());
+		assertEquals("* ログインに失敗しました。", idErrorElement.getText());
+		assertEquals("ログイン | LMS", webDriver.getTitle());
 	}
 
 }
